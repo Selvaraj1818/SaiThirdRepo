9f4f462ccd3a5a18a2146006c32230270f39e7f5
RajSelva1881 <rajselva1881@yahoo.com>
2024-04-08 21:01:12 -0700

    First Commit

---
+++ studentEnrollment/src/main/java/com/example/studentEnrollment/StudentEnrollmentApplication.java
@@ -0,0 +1,19 @@
+package com.example.studentEnrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class StudentEnrollmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentEnrollmentApplication.class, args);
+		System.out.println("Started");
+	}
+
+}
+
+// Comment 1
